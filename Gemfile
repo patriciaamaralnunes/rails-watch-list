94c4d943ad375b97523c35b59d9c6fefc3b74c6e
Patricia Nunes <patricia.amaral.nunes@hotmail.com>
2021-05-28 12:42:35 +0100

    bundlecenas

--- Gemfile
+++ Gemfile
@@ -27,6 +27,10 @@ gem 'jbuilder', '~> 2.7'
 
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.2', require: false
+gem 'autoprefixer-rails', '10.2.5'
+gem 'font-awesome-sass', '~> 5.12.0'
+gem 'simple_form'
+gem 'rest-client'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Deleting everything..."
+Bookmark.destroy_all
+Movie.destroy_all
+
+puts "Creating movies..."
+url = 'http://tmdb.lewagon.com/movie/top_rated'
+
+movies_json = JSON.parse(RestClient.get(url).body, symbolize_names: true)
+
+movies_json[:results].each do |movie_json|
+  Movie.create!(
+    title: movie_json[:title],
+    overview: movie_json[:overview],
+    poster_url: "https://image.tmdb.org/t/p/w500/#{movie_json[:poster_path]}",
+    rating: movie_json[:vote_average]
+  )
+end
+
+puts "Done 🎉"
